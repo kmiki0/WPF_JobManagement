8d866db557d813f14dc2026b3b81b05ebb1a7ec4
kmiki0 <kmiki@anvil.ne.jp>
2025-06-16 00:58:51 +0900

    JobLogItemViewModelの設定を一部修正

--- JobManagementApp/ViewModels/JobListItemViewModel.cs
+++ JobManagementApp/ViewModels/JobListItemViewModel.cs
@@ -1,135 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using System.Windows.Input;
-using JobManagementApp.Commands;
-using JobManagementApp.Views;
-using JobManagementApp.Services;
-using System.Windows;
+﻿using JobManagementApp.Commands;
 using JobManagementApp.Models;
+using System;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace JobManagementApp.ViewModels
 {
-    public class JobListItemViewModel : INotifyPropertyChanged
+    public class JobLogItemViewModel : INotifyPropertyChanged
     {
         // イベント処理
-        private readonly JobListItemCommand _command;
+        private readonly JobLogItemCommand _command;
 
-        // シナリオ
+        // 静的プロパティ（初期設定後変更されない）
         public string Scenario { get; set; }
-        // 枝番
         public string Eda { get; set; }
-        // ジョブID
-        private string _id { get; set; }
-        public string Id {
-            get => _id;
+        public string Id { get; set; }
+        public emFileType FileType { get; set; }
+        public int FileCount { get; set; }
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public int ObserverType { get; set; }
+        
+        // 画面表示用ファイル名
+        private string _displayFileName;
+        public string DisplayFileName 
+        {
+            get => _displayFileName;
             set
             {
-                _id = value;
-                OnPropertyChanged(nameof(Id));
+                if (_displayFileName != value)
+                {
+                    _displayFileName = value;
+                    OnPropertyChanged(nameof(DisplayFileName));
+                }
             }
         }
-        // ジョブ名
-        private string _name { get; set; }
-        public string Name {
-            get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
-        // ジョブ実行方法
-        private emExecution _execution { get; set; }
-        public emExecution Execution {
-            get => _execution;
-            set
-            {
-                _execution = value;
-                OnPropertyChanged(nameof(Execution));
-            }
-        }
-        // 実行ボタンの活性・非活性
-        private bool _jobBoolean{ get; set; }
-        public bool JobBoolean {
-            get => _jobBoolean;
-            set
-            {
-                _jobBoolean = value;
-                OnPropertyChanged(nameof(JobBoolean));
-            }
-        }
-        // ジョブステータス
-        private emStatus _status { get; set; }
-        public emStatus Status {
-            get => _status;
-            set
-            {
-                _status = value;
-                OnPropertyChanged(nameof(Status));
-            }
-        }
-        // 更新日付
-        private string _updateDate { get; set; }
-        public string UpdateDate {
+
+        // 更新日時
+        private string _updateDate;
+        public string UpdateDate 
+        {
             get => _updateDate;
             set
             {
-                _updateDate = value;
-                OnPropertyChanged(nameof(UpdateDate));
+                if (_updateDate != value)
+                {
+                    _updateDate = value;
+                    OnPropertyChanged(nameof(UpdateDate));
+                }
             }
         }
-        // TreeViewが開いているか
-        private bool _isOpenTreeView { get; set; }
-        public bool IsOpenTreeView {
-            get => _isOpenTreeView;
+
+        // 行数
+        private string _lineCount;
+        public string LineCount 
+        {
+            get => _lineCount;
             set
             {
-                _isOpenTreeView = value;
-                OnPropertyChanged(nameof(IsOpenTreeView));
+                if (_lineCount != value)
+                {
+                    _lineCount = value;
+                    OnPropertyChanged(nameof(LineCount));
+                }
             }
         }
 
-        // シナリオグループ（これで、TreeView上のシナリオのボタンやテキストを非表示にする）
-        public bool IsScenarioGroup { get; set; }
-        // 入れ子リスト
-        public ObservableCollection<JobListItemViewModel> Children { get; set; }
-
-        // 実行 ボタン
-        public ICommand RunCommand { get; set; }
-        // 詳細 ボタン
-        public ICommand DetailCommand { get; set; }
-        // ログ ボタン
-        public ICommand LogCommand { get; set; }
-
-        /// <summary>
-        /// Init
-        /// </summary>
-        public JobListItemViewModel()
+        // ファイルサイズ
+        private string _size;
+        public string Size 
         {
-            // 初期値 セット
-            IsOpenTreeView = false;
-            IsScenarioGroup = false;
-            UpdateDate = "";
-            Children = new ObservableCollection<JobListItemViewModel>();
-
-            // ボタンイベント 初期化
-            _command = new JobListItemCommand(this);
-            RunCommand = new RelayCommand(_command.RunButton_Click);
-            DetailCommand = new RelayCommand(_command.DetailButton_Click);
-            LogCommand = new RelayCommand(_command.LogButton_Click);
+            get => _size;
+            set
+            {
+                if (_size != value)
+                {
+                    _size = value;
+                    OnPropertyChanged(nameof(Size));
+                }
+            }
         }
 
-        // VM変更検知
+        // 監視ステータス
+        private emObserverStatus _observerStatus;
+        public emObserverStatus ObserverStatus 
+        {
+            get => _observerStatus;
+            set
+            {
+                if (_observerStatus != value)
+                {
+                    _observerStatus = value;
+                    OnPropertyChanged(nameof(ObserverStatus));
+                }
+            }
+        }
+
+        // ファイルコピー進行率
+        private string _copyPercent;
+        public string CopyPercent 
+        {
+            get => _copyPercent;
+            set
+            {
+                if (_copyPercent != value)
+                {
+                    _copyPercent = value;
+                    OnPropertyChanged(nameof(CopyPercent));
+                }
+            }
+        }
+
+        // コマンド
+        public ICommand EditCommand { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public JobLogItemViewModel()
+        {
+            // コマンド初期化
+            _command = new JobLogItemCommand(this);
+            EditCommand = new RelayCommand(_command.EditButton_Click);
+            
+            // 初期値設定
+            _displayFileName = "";
+            _updateDate = "";
+            _lineCount = "";
+            _size = "";
+            _copyPercent = "";
+            _observerStatus = emObserverStatus.STOP;
+        }
+
+        // INotifyPropertyChanged実装
         public event PropertyChangedEventHandler PropertyChanged;
+        
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
 }
